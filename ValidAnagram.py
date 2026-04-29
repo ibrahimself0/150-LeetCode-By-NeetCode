2b8d4177bf43cb928ca640c59123534011a9ad2c
IBRA <ibrahimalebrahim17@gmail.com>
2026-04-29 14:14:05 +0300

    first commit

---
+++ ValidAnagram.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if(len(s)!=len(t)):
+            return False
+        for char in s:
+            if char not in t:
+                return False
+            else:
+                t = t.replace(char,"",1);
+                print(t)
+        return True
+
